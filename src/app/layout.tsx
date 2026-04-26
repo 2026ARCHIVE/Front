26441cfe9628370775f85dfcada855456bc031bf
DaeHun Kang <crol0101@naver.com>
2026-04-26 22:41:46 +0900

    feat : 글로벌 레이아웃 및 헤더, 네비바 추가

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,5 +1,27 @@
-import React from "react";
+import type { Metadata } from "next";
+import "./globals.css";
+import Header from "@/components/Header";
+import BottomNavBar from "@/components/navbar/BottomNavBar";
 
-export default function Layout() {
-  return <div>Layout</div>;
+export const metadata: Metadata = {
+  title: "SMU 26 대동제",
+  description: "2026 대동제 축제 웹사이트입니다.",
+};
+
+export default function RootLayout({
+  children,
+}: {
+  children: React.ReactNode;
+}) {
+  return (
+    <html lang="ko">
+      <body className="bg-gray-100 antialiased">
+        <div className="mx-auto flex min-h-screen max-w-md flex-col bg-white shadow-lg relative ">
+          <Header />
+          <main className="flex-1 overflow-y-auto pb-16">{children}</main>
+          <BottomNavBar />
+        </div>
+      </body>
+    </html>
+  );
 }
---
+++ src/components/Header.tsx
@@ -0,0 +1,75 @@
+"use client";
+import { usePathname, useRouter } from "next/navigation";
+import React, { use } from "react";
+
+export default function Header() {
+  const pathname = usePathname();
+  const router = useRouter();
+
+  const getHeaderTitle = () => {
+    switch (pathname) {
+      case "/":
+        return "홈";
+      case "/booths":
+        return "부스";
+      case "/map":
+        return "지도";
+      case "/schedule":
+        return "일정";
+      case "/notice":
+        return "공지";
+      case "/not-found":
+        return "에러";
+      case "/event":
+        return "이벤트";
+      case "/lost-and-found":
+        return "분실물";
+      case "/goods":
+        return "굿즈";
+      case "/event/[id]":
+        return "이벤트 상세";
+      case "/booths/[id]":
+        return "부스 상세";
+      case "/lost-and-found/[id]":
+        return "분실물 상세";
+      case "/goods/[id]":
+        return "굿즈 상세";
+      default:
+        return "대동제 2026";
+    }
+  };
+
+  if (pathname === "/") {
+    return null;
+  }
+  return (
+    <header className="sticky top-0 z-50 flex w-full max-w-md items-center justify-between bg-white px-5 pb-5 pt-18.75 h-31.25">
+      <button
+        onClick={() => router.back()}
+        className="flex h-8 w-8 items-center justify-center rounded-full text-custom-gray hover:bg-gray-100 transition-colors"
+        aria-label="뒤로 가기"
+      >
+        <svg
+          width="9"
+          height="16"
+          viewBox="0 0 9 16"
+          fill="none"
+          xmlns="http://www.w3.org/2000/svg"
+        >
+          <path
+            d="M7.75 14.75L0.75 7.75L7.75 0.75"
+            stroke="black"
+            strokeWidth="1.5"
+            strokeLinecap="round"
+            strokeLinejoin="round"
+          />
+        </svg>
+      </button>
+      <h1 className="flex-1 text-center text-[20px] font-semibold text-black">
+        {getHeaderTitle()}
+      </h1>
+
+      <div className="h-8 w-8"></div>
+    </header>
+  );
+}
