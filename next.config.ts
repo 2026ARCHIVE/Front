8e246ebc79644a12afc106270524949c01a86517
DaeHun Kang <crol0101@naver.com>
2026-04-27 15:50:50 +0900

    dev빌드 cors에러 해결

--- next.config.ts
+++ next.config.ts
@@ -3,6 +3,7 @@ import type { NextConfig } from "next";
 const nextConfig: NextConfig = {
   /* config options here */
   reactCompiler: true,
+  allowedDevOrigins: ["http://192.168.0.6:3000", "192.168.0.6"],
 };
 
 export default nextConfig;
