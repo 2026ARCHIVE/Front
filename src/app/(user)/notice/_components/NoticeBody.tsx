379593f5be40b86d2922f241238f99740605abca
DaeHun Kang <crol0101@naver.com>
2026-05-12 17:40:40 +0900

    feat : 공지사항 api 연동

--- src/app/(user)/notice/_components/NoticeBody.tsx
+++ src/app/(user)/notice/_components/NoticeBody.tsx
@@ -2,70 +2,49 @@
 import React, { useEffect, useState } from "react";
 import NoticeItem from "./NoticeItem";
 import { Loader2 } from "lucide-react";
-
-interface Notice {
-  id: number;
-  title: string;
-  date: Date;
-  detail: string;
-}
-const dummyData: Notice[] = [
-  {
-    id: 1,
-    title: "공지사항 1",
-    date: new Date(),
-    detail: "공지사항 내용 1",
-  },
-  {
-    id: 2,
-    title: "공지사항 2",
-    date: new Date(),
-    detail: "공지사항 내용 2",
-  },
-  {
-    id: 3,
-    title: "공지사항 3",
-    date: new Date(),
-    detail: "공지사항 내용 3",
-  },
-  {
-    id: 4,
-    title: "공지사항 4",
-    date: new Date(),
-    detail: "공지사항 내용 4",
-  },
-  {
-    id: 5,
-    title: "공지사항 5",
-    date: new Date(),
-    detail: "공지사항 내용 5",
-  },
-  {
-    id: 6,
-    title: "공지사항 6",
-    date: new Date(),
-    detail: "공지사항 내용 6",
-  },
-];
+import { getNotices, Notice } from "@/api/notice";
 
 export default function NoticeBody() {
   const [notices, setNotices] = useState<Notice[]>([]);
+  const [loading, setLoading] = useState(true);
+
   useEffect(() => {
-    const timer = setTimeout(() => {
-      setNotices(dummyData);
-    }, 1000);
-    return () => clearTimeout(timer);
+    const fetchNotices = async () => {
+      setLoading(true);
+      const { ok, data } = await getNotices();
+
+      if (ok && data) {
+        if (process.env.NODE_ENV === "development") {
+          console.log("공지사항 목록:", data);
+        }
+        setNotices(data);
+      } else {
+        console.error("공지사항 목록을 불러오지 못했습니다.");
+      }
+      setLoading(false);
+    };
+
+    fetchNotices();
   }, []);
 
-  if (notices.length === 0) {
+  if (loading) {
     return (
-      <div className="flex-1 flex items-center justify-center">
+      <div className="flex-1 flex items-center justify-center min-h-50">
         <Loader2 className="h-8 w-8 animate-spin text-custom-blue" />
       </div>
     );
   }
+
+  if (notices.length === 0) {
+    return (
+      <div className="flex-1 flex items-center justify-center text-gray-500">
+        등록된 공지사항이 없습니다.
+      </div>
+    );
+  }
+
   return (
-    <div className=" relative flex-1">
+    <div className="relative flex-1">
       {notices.map((notice) => (
         <NoticeItem key={notice.id} notice={notice} />
       ))}
