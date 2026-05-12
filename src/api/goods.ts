c21e08cebee1d6de729ba3d6965a7b477b8a2798
DaeHun Kang <crol0101@naver.com>
2026-05-12 18:12:54 +0900

    feat : 굿즈 페이지 api 연동

---
+++ src/api/goods.ts
@@ -0,0 +1,25 @@
+import { apiFetchJson } from "./client";
+
+export interface Goods {
+  id: number;
+  name: string;
+  description: string;
+  price: number;
+  location: string;
+  status: "ON_SALE" | "SOLD_OUT";
+  imageUrls: string[];
+  createdAt: string;
+}
+
+export interface GoodsDetail extends Goods {
+  salesOpenTime: string;
+  salesCloseTime: string;
+}
+
+export async function getGoods() {
+  return await apiFetchJson<Goods[]>("/goods", { cache: "no-store" });
+}
+
+export async function getGoodsById(id: number) {
+  return await apiFetchJson<GoodsDetail>(`/goods/${id}`, { cache: "no-store" });
+}
--- src/app/(user)/goods/[id]/_components/GoodsDetailBody.tsx
+++ src/app/(user)/goods/[id]/_components/GoodsDetailBody.tsx
@@ -3,67 +3,10 @@ import Image from "next/image";
 import React, { useEffect, useState } from "react";
 import GoodsDetailBoard from "./GoodsDetailBoard";
 import { Loader2 } from "lucide-react";
-
-export interface GoodsDetail {
-  id: number;
-  name: string;
-  description: string;
-  price: number;
-  imageUrl: string;
-  isSoldOut: boolean;
-  salesTime: string;
-  place?: string;
-}
-
-const dummyData: GoodsDetail[] = [
-  {
-    id: 1,
-    name: "유니폼",
-    description:
-      "상명대 대동제의 시그니처 색인 하늘색과 요즘 트렌드인 유님폼을 조합해서 제작한 유니폼.",
-    price: 4500,
-    imageUrl: "/Goods/goods1_detail.png",
-    isSoldOut: false,
-    salesTime: "10:00 - 22:00",
-    place: "디자인대학 4층",
-  },
-  {
-    id: 2,
-    name: "반다나",
-    description:
-      "상명대 대동제의 시그니처 색인 하늘색과 요즘 트렌드인 유님폼을 조합해서 제작한 반다나.",
-    price: 1000,
-    imageUrl: "/Goods/goods2_detail.png",
-    isSoldOut: true,
-    salesTime: "10:00 - 22:00",
-    place: "디자인대학 4층",
-  },
-  {
-    id: 3,
-    name: "타투 스티커",
-    description:
-      "상명대 대동제의 시그니처 색인 하늘색과 요즘 트렌드인 유님폼을 조합해서 제작한 타투 스티커.",
-    price: 2000,
-    imageUrl: "/Goods/goods3_detail.png",
-    isSoldOut: false,
-    salesTime: "10:00 - 22:00",
-    place: "디자인대학 4층",
-  },
-  {
-    id: 4,
-    name: "핀버튼",
-    description:
-      "상명대 대동제의 시그니처 색인 하늘색과 요즘 트렌드인 유님폼을 조합해서 제작한 핀버튼.",
-    price: 3000,
-    imageUrl: "/Goods/goods4_detail.png",
-    isSoldOut: true,
-    salesTime: "10:00 - 22:00",
-    place: "디자인대학 4층",
-  },
-];
+import { GoodsDetail, getGoodsById } from "@/api/goods";
 
 export default function GoodsDetailBody({ goodsId }: { goodsId: string }) {
-  const [goodsDetail, setGoodsDetail] = useState<GoodsDetail>();
+  const [goodsDetail, setGoodsDetail] = useState<GoodsDetail | null>(null);
   const [isLoading, setIsLoading] = useState(true);
 
   useEffect(() => {
@@ -72,10 +15,21 @@ export default function GoodsDetailBody({ goodsId }: { goodsId: string }) {
       setIsLoading(true);
 
       try {
-        await new Promise((resolve) => setTimeout(resolve, 1000));
-
-        const detail = dummyData.find((item) => item.id === parseInt(goodsId));
-        setGoodsDetail(detail);
+        const { status, ok, data } = await getGoodsById(parseInt(goodsId));
+        if (ok && data) {
+          if (process.env.NODE_ENV === "development") {
+            console.log("상품 상세 정보:", data);
+          }
+          setGoodsDetail(data);
+        }
+        if (status === 404) {
+          setGoodsDetail(null);
+          if (process.env.NODE_ENV === "development") {
+            console.warn(
+              `상품 ID ${goodsId}에 대한 상세 정보를 찾을 수 없습니다.`,
+            );
+          }
+        }
       } catch (error) {
         console.error("데이터를 가져오는데 실패했습니다.", error);
       } finally {
@@ -99,17 +53,26 @@ export default function GoodsDetailBody({ goodsId }: { goodsId: string }) {
       </div>
     );
 
+  const mainImageUrl =
+    goodsDetail.imageUrls?.length > 1
+      ? goodsDetail.imageUrls[1]
+      : goodsDetail.imageUrls?.[0] || null;
+
   return (
     <div className="flex flex-col items-center gap-10">
-      <Image
-        src={goodsDetail.imageUrl}
-        alt={goodsDetail.name}
-        width={400}
-        height={400}
-        priority
-        className="mt-4 w-auto h-auto"
-      />
-      <GoodsDetailBoard {...goodsDetail} />
+      {mainImageUrl ? (
+        <Image
+          src={mainImageUrl}
+          alt={goodsDetail.name}
+          width={400}
+          height={400}
+          priority
+          className="mt-4 w-auto h-auto max-w-100 max-h-100 object-cover"
+        />
+      ) : (
+        <div className="mt-4 w-100 h-100 bg-gray-200" />
+      )}
+      <GoodsDetailBoard goods={goodsDetail} />
     </div>
   );
 }
