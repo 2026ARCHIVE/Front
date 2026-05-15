ebb35650e5b0008003682a03532f1e5bbdaf2791
chacha <chayeonggeon@gmail.com>
2026-05-15 18:07:23 +0900

    chore: 부스 필터 아이템 폰트 크기 조정

--- src/app/(user)/booths/_components/FilterItem.tsx
+++ src/app/(user)/booths/_components/FilterItem.tsx
@@ -13,7 +13,7 @@ export default function FilterItem({
 }: FilterItemProps) {
   return (
     <div
-      className={`px-4 py-2 shrink-0 whitespace-nowrap rounded-4xl cursor-pointer ${
+      className={`px-4 py-2 shrink-0 whitespace-nowrap rounded-4xl cursor-pointer text-[16px] font-semibold ${
         isActive
           ? "bg-custom-blue text-white font-semibold"
           : "bg-transparent text-custom-darkgray"
