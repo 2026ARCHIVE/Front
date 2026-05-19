ab5c447c5c963254ff4f966f5bdec697467388de
chacha <chayeonggeon@gmail.com>
2026-05-19 11:31:04 +0900

    chore: 팝업 내용 수정

--- src/app/(user)/about/_components/Description.tsx
+++ src/app/(user)/about/_components/Description.tsx
@@ -9,44 +9,27 @@ export default function Description() {
           Deer for U: ARCHIVE
         </h1>
         <p className="text-[16px] text-custom-darkgray ">
-          2026.05.23(목) - 24(금)
+          2026.05.27(수) - 28(목)
           <br />
           상명대학교 천안캠퍼스 일대
         </p>
       </div>
-
       <p className="bg-custom-lightgray p-2.5 text-[13px] text-custom-darkgray">
-        상명대학교 2026년도 대동제의 컨셉은 ‘Archive’로 보관이라는 무엇아래에
-        도장을찍어 무언가. 캡션을 작성해주시면 됩니다.
+        이번 대동제 &apos;Deer For U_ARCHIVE&apos;는 쉼 없이 흐르는 시간 속에서
+        지금 이 순간, 오직 우리만이 만들어 낼 수 있는 찰나를 놓치지 않고 영원한
+        기록으로 남기고자 기획되었습니다.
         <br />
         <br />
-        분량은 약 2문단 정도 나오게 해서 제작국이나 그런 곳에서 작성해주시면 될
-        것 같습니다.
+        축제의 정체성을 담은 메인 타이포그래피 ‘Archive’는 학생들의 멈추지 않는
+        활력과 폭발적인 에너지를 상징하는 불꽃 그래픽을 타이틀과 결합하여,
+        우리의 열정이 하나의 기록으로 완성되는 과정을 시각화 했습니다
+        <br />
+        <br />
+        각자의 장면들이 모여 하나의 거대한 이야기가 될 때, 비로소 우리만의
+        &apos;아카이브&apos;는 완성됩니다. 축제가 끝난 뒤에도 가슴 한구석에
+        선명하게 남아있을 그날의 기억들이 여러분의 인생에서 가장 빛나는 페이지로
+        기록되기를 바랍니다.
       </p>
-      <div className="flex flex-col gap-2.5">
-        <h2 className="font-bold text-[13px] text-black ">
-          대동제 참여 유의사항
-        </h2>
-
-        <ul className="list-disc pl-4 flex flex-col gap-0.75 text-[12px] text-custom-darkgray marker:text-custom-darkgray">
-          <li>
-            부상 예방을 위해 참가자는 빠르게 달리거나 격렬하게 움직일 때 주의
-          </li>
-          <li>농부 역할을 맡은 사람은 과도하게 힘을 주지 않도록 주의</li>
-          <li>체험 전, 게임 규칙과 안전 수칙을 참가자들에게 충분히 안내</li>
-          <li>
-            농부는 도망자와 적당한 거리를 두고 추격하며, 다칠 위험을 줄인다
-          </li>
-          <li>
-            게임 중 수박이 터지거나 파손되지 않도록 조심하고, 게임 종료 후
-            청소는 반드시 진행
-          </li>
-          <li>
-            더운 날씨에는 음료수 및 그늘막을 준비해 참가자들이 건강을 유지할 수
-            있도록 한다
-          </li>
-        </ul>
-      </div>
     </div>
   );
 }
